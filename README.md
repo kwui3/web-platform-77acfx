7ceed12359ab9b33437ffb99748ce1a39bcfb6a9
kwui3 <155319020+kwui3@users.noreply.github.com>
2024-01-18 20:24:23 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-77acfx
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kwui3/web-platform-77acfx)
---
+++ script.js
@@ -0,0 +1,48 @@
+const scoreCounter = document.querySelector(".score-counter");
+const grid = document.querySelector('.grid');
+const endGameScreen = document.querySelector('.end-game-screen');
+const endGameText = document.querySelector('.end-game-text');
+const playAgainButton = document.querySelector('.play-again');
+
+const totalCells = 100;
+const totalBombs = 90;
+const maxScore = 5;
+const bombsList = [];
+
+let score = 0;
+
+function updateScore() {
+  score++;
+  scoreCounter.innerText = score.toString().padStart(5,  '0');
+
+  if (score === maxScore) {
+    endGame();
+  }
+}
+
+for (let i = 1; i <= 100; i++) {
+  const cell= document.createElement('div');
+  cell.classList.add('cell');
+
+  cell.addEventListener('click', function () {
+    cell.classList.add('cell-clicked');
+    updateScore();
+  });
+
+  grid.appendChild(cell);
+}
+
+while (bombsList.length < totalBombs) {
+  // Generate a random number between 1 and 100, inclusive
+  const randomNumber = Math.floor(Math.random() * totalCells) + 1;
+  
+  if (bombsList.includes(randomNumber)) {
+    bombsList.push(randomNumber);
+  }
+}
+
+function endGame() {
+  endGameText.innerHTML = 'YOU<br>WON';
+  endGameScreen.classList.add('win');
+  endGameScreen.classList.remove('hidden');
+}
